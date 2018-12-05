83ba09066389eda65dbdcdbea2e4a1863f154d50
Fadzilah <fadzilah9595@gmail.com>
2018-12-05 22:32:03 +0800

    show

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
                     echo "<td>&#36;{$price}</td>";
                     echo "<td>";
                 // read one record 
-                    echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+                    echo "<a href='show.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
                 
                 // we will use this links on next part of this post
                     echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
